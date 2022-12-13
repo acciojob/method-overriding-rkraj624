d838e8f5ab9ed69af98618b235beb5aafa7f0436
Ravi Kishan Raj <ravikishan624@.com>
2022-12-13 13:25:50 +0530

    final submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         A obj = new B();
-//        System.out.println(obj.meth());
         obj.meth();
+//        System.out.println(obj.meth());
 //        B obj1 = new B();
 //        obj1.meth();
 //        System.out.println(obj1.meth());
